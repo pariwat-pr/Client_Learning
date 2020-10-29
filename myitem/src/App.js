00917e91b51056f957596a95d50de4890bb8a0f0
Pariwat Prathanrat <pariwat@Pariwats-MacBook.local>
2020-10-29 17:57:41 +0700

    v1

---
+++ myitem/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <Fragment>
+      <p>Hello world</p>
+    </Fragment>
+  );
+};
+
+export default App;
